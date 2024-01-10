b33688ce6ca971d713981d767903948d06ef9f31
Desenvolvimento <desenvolvimento@dip.com>
2024-01-10 17:38:38 -0300

    chore: add ts-jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,10 +2,10 @@ import type { Config } from 'jest'
 
 const config: Config = {
   roots: ['<rootDir>/src'],
-  collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  collectCoverage: true,
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
