d9b850e26f014a9782e2e37036c6d2b2ae2342a4
Desenvolvimento <desenvolvimento@dip.com>
2024-02-05 08:49:21 -0300

    feat: ensure SignUpController returns 200 if valid email is provided

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -32,14 +32,14 @@ export class SignUpController implements Controller {
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       } else {
-        this.addAccount.add({
+        const account = this.addAccount.add({
           name,
           email,
           password
         })
         return {
           statusCode: 200,
-          body: httpRequest.body.json()
+          body: account
         }
       }
     } catch (error) {
