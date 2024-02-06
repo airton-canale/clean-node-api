17183d339cbb97ab7f1923b1e629bc5f654afb53
Airton <airtonluizcanalefilho@hotmail.com>
2024-02-06 09:19:19 -0300

    fix: resolve conflicts to merge

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -2,6 +2,14 @@ import { HttpResponse } from '../protocols/http'
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400, body: error
 })
+
+export class ServerError extends Error {
+  constructor () {
+    super('Internal server error')
+    this.name = 'ServerError'
+  }
+}
+
 export const ok = (data: any): HttpResponse  => ({
   statusCode: 200,
   body: data
